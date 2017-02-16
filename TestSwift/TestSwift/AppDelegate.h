a284fa19db7c3eaff4298ba4f339dc53584254cc
huyiyong <991779153@qq.com>
2017-02-16 22:36:24 +0800

    创建swift

---
+++ TestSwift/TestSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSwift
+//
+//  Created by huyiyong on 17/2/16.
+//  Copyright © 2017年 huyiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
