ca7266491c10f92b92bfcea2b935a1682d4d800a
huyiyong <991779153@qq.com>
2017-02-19 20:52:10 +0800

    SWIFT

--- TestSwiftCase/TestSwiftCase/ViewController.swift
+++ TestSwiftCase/TestSwiftCase/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var myCode: String
+        myCode = "nihao";
+        
     }
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
 
